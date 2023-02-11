ba73f76bf4da6477f3e68f61aee18ba6ef8df823
Viktoria <viktoriaani22@gmail.com>
2023-02-11 13:48:11 +0200

    Extract and move to new file LanguageViewCellController

--- EcoFlow/Settings/Controllers/TableViewCellController/TableViewCellController.swift
+++ EcoFlow/Settings/Controllers/TableViewCellController/TableViewCellController.swift
@@ -25,23 +25,4 @@ extension TableViewCellController {
 
 
 
-class LanguageViewCellController: TableViewCellController {
 
-    private let model: Language
-
-    init(model: Language) {
-        self.model = model
-    }
-//
-    static func configure(tableView: UITableView) {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "languageCell")
-    }
-    
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "languageCell")
-        cell.textLabel?.text = model.name
-        cell.backgroundColor = .clear
-        return cell
-    }
-}
