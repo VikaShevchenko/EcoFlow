de5ccdb5807e936318061f879bd1f5e49ac97d45
Viktoria <viktoriaani22@gmail.com>
2023-02-11 13:47:17 +0200

    Extract and move to new file UserTableViewCellController

--- EcoFlow/Settings/Controllers/TableViewCellController/TableViewCellController.swift
+++ EcoFlow/Settings/Controllers/TableViewCellController/TableViewCellController.swift
@@ -22,26 +22,7 @@ extension TableViewCellController {
 
 
 
-class UserTableViewCellController: TableViewCellController {
-    
-    private let model: User
-    
-    init(model: User) {
-        self.model = model
-    }
-    
-    static func configure(tableView: UITableView) {
-        tableView.register(UserTableViewCell.self)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: UserTableViewCell.reuseIdentifier, for: indexPath) as! UserTableViewCell
-        cell.userImageView.image = UIImage(named: model.imageName)
-        cell.userNameLabel.text = model.name
-        cell.userEmailLabel.text = model.email
-        return cell
-    }
-}
+
 
 
 class LanguageViewCellController: TableViewCellController {
